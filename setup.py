b6f001428ee6d1c5dd76c6a8d90638fab900aa8b
Pavlo Pikuza <pavlo.pikuza@gmail.com>
2025-02-21 02:23:30 -0700

    Fixed package structure: removed src/

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='math-utils',
     version='0.1.0',
-    packages=find_packages(where = "src"),
-    package_dir={"": "src"},
+    packages=['math_utils'],
+    package_dir={"math_utils": "math_utils"},
     install_requires=[],
     description='A collection of mathematical utilities',
     author='Pavel Pikuza',
