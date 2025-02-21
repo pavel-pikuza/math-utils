82693f8a7e3fd96ea0ce6eb58f03a0529f50e772
Pavlo Pikuza <pavlo.pikuza@gmail.com>
2025-02-21 02:16:36 -0700

    Updated __init__.py for direct imports

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='math-utils',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(where = "src"),
+    package_dir={"": "src"},
     install_requires=[],
     description='A collection of mathematical utilities',
     author='Pavel Pikuza',
